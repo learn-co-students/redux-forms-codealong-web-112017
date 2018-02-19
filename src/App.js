e7918bb88b99efb3791fa430b03d0bcec1ca9583
Flatiron School <leslie.hsu@flatironschool.com>
2018-02-18 21:17:22 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CreateTodo from './components/todos/CreateTodo'
 
 class App extends Component {
   render() {
+    console.log('re-rendered app', this.props.store.getState())
     return (
       <div className="App">
         <CreateTodo store={this.props.store} />
--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+
+  handleInput = (e) => {
+    this.setState({text:e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch({type: 'add_todo', todo: this.state.text})
+    debugger
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form  onSubmit={this.handleSubmit}>
+          <input type='text' placeholder='enter new to do here' onChange={this.handleInput} value={this.state.text}></input>
+          <input type='submit'></input>
+        </form>
       </div>
     )
   }
